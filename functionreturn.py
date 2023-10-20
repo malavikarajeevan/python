434aeda8eeb2f82aba539429d3f1821e2add70a7
malavika <malavikarajeevan85@gmail.com>
2023-10-20 09:00:52 +0530

    file added

--- functionreturn.py
+++ functionreturn.py
@@ -49,6 +49,25 @@
 # else:
 #     print("false")
 
+#local and global
+
+# a=10
+# def asd():
+#     print("inside asd()",10)
+# asd()
+# print("outside asd()",10)
 
 
+# def bsd():
+#     a=10
+# bsd()
+# print("outside bsd",10)
+
+# def asd():
+#     a=10
+#     def bsd():
+#         print("inside asd",a)
+#         bsd()
+#     asd()
+#     print("outside bsd",a)
 
---
+++ recursive.py
@@ -0,0 +1,29 @@
+# a=1
+# def asd(b):
+#     if b<=10:
+#         print(b)
+#         b=b+1
+#         asd(b)
+# asd(a)
+
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+# res=fact(8)
+# print(res)
+
+
+a=100
+def asd(n):
+    if n>=1:
+        print(n)
+        n=n-1
+        asd(n)
+asd(a)
+    
+
+    
+
+
