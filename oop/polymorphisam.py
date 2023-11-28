19fc7a688837b80fdd0b8646cc0006508555c363
malavika <malavikarajeevan85@gmail.com>
2023-11-28 09:18:26 +0530

    new file added

---
+++ oop/polymorphisam.py
@@ -0,0 +1,31 @@
+"""""method overloading"""
+
+# class maths:
+#     def add(self,a,b):
+#         print(a+b)
+#     def add(self,a,b,c=0):
+#         print(a+b+c)
+# ob=maths()
+# ob.add(5,3)
+# ob.add(5,3,2)
+
+""""method overiding"""
+# class kozhikode:
+#     def place(self):
+#         print("vadakara")
+# class orkkatteri(kozhikode):
+#     def place(self):
+#         print("vattoli")
+# class kuttyadi(kozhikode):
+#     def place(self):
+#         print("nadapuram")
+# a=kozhikode()
+# b=orkkatteri()
+# c=kuttyadi()
+# a.place()
+""""odd or evern with out using modulas"""
+# num=int(input("enter a number"))
+# if (num & 1)==1:
+#     print("it is an odd number")
+# else:
+#     print("it is an evern number")
---
+++ oop/regular exp.py
@@ -0,0 +1,73 @@
+# import re
+# terms=" "
+# x=re.findall(terms,"synnefo hello hello")
+# x=re.split(terms,"synnefo hello hello")
+# x=re.match(terms," hello synnefo")
+# x=re.search(terms,"synnefo hello hello")
+# print(x)
+
+
+# import re
+# terms='[A-Za-z0-9]'
+# x=re.match(terms,"7aswanth")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
+    
+    
+# import re
+# terms='[a-z]......'
+# x=re.match(terms,"aswanth")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
+    
+    
+# import re
+# terms='^aswanth'
+# x=re.match(terms,"aswanthhhh")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
+    
+    
+    
+# import re
+# terms='aswanth$'
+# x=re.search(terms,"aswanth")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
+
+    
+# import re
+# terms='aswanth?'
+# x=re.match(terms,"aswanthhh")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
+
+
+    
+# import re
+# terms='aswanth*'
+# x=re.match(terms,"aswan")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
+
+
+    
+# import re
+# terms='aswanth+'
+# x=re.match(terms,"aswanth")
+# if x:
+#     print("matched")
+# else:
+#     print("not matched")
