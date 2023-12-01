2861f8ac32111f8623d3271a708d31e426bf01c9
malavika <malavikarajeevan85@gmail.com>
2023-12-01 09:06:39 +0530

    new file added

--- oop/regular exp.py
+++ oop/regular exp.py
@@ -122,14 +122,83 @@
 
 
 
-import re
-day=str(input("enter the date:"))
+# import re
+# day=str(input("enter the date:"))
 
-terms="^([^00]|[0][1,9]|[12][1,9]|3[1]|[123][0])(-|.|/)([^00]|[0][1,9]|[012])(-|.|/)([^0000]\d{4})$"
-b=re.search(terms,"11/11/0000")
-if b:
-    print("validate")
+# terms="^([^00]|[0][1,9]|[12][1,9]|3[1]|[123][0])(-|.|/)([^00]|[0][1,9]|[012])(-|.|/)([^0000]\d{4})$"
+# b=re.search(terms,"11/11/0000")
+# if b:
+#     print("validate")
+# else:
+#     print("not validate")
+
+
+# import re
+# email=str(input("enter your email:"))
+# terms="^[a-z0-9]+[\|.|-]?[a-z0-9]+@\w+[.]\w{2,3}$"
+# b=re.search(terms,"email")
+# if b:
+#     print("validate")
+# else:
+#     print("not validate")
+
+
+
+# import re
+# a=str(input("enter your email:"))
+# terms="^[A-Za-z].+@[A-Za-z]{2,}\.[A-Za-z]{1,}&"
+# b=re.search(terms,a)
+# if b:
+#     print("validate")
+# else:
+#     print("not validate")
+
+
+""""password"""
+# import re
+
+# terms="^[A-Z]\w{3,}\W\w{2,}$"
+# a=str(input("enter your password:"))
+# b=re.search(terms,a)
+# if b:
+#     print("validate")
+# else:
+#     print("not validate")"^[A-Za-z].+@[A-Za-z]{2,}\.[A-Za-z]{1,}&"
+
+
+# import re
+# text="the rain in spain"
+# x=re.findall("[aeiou]\w",text)
+# print(x)
+# if x:
+#     print("yes there is a match")
+# else:
+#     print("no match")
+
+
+# import re
+# terms="a|e|i|o|u"
+# a=str(input("enter the word:"))
+# x=re.findall(terms,a)
+# print(x)
+# print(len(x))
+""""email&password"""
+import re
+terms="^[A-Z]\w{3,}\W\w{2,}$"
+a=input("enter the password:")
+x=re.search(terms,a)
+if x:
+    print("password validate")
 else:
-    print("not validate")
+    print("password is not validate")
+import re
+
+terms="^[A-Za-z].+@[A-Za-z]{2,}\.[A-Za-z]{1,}&"
+b=input("enter the email:")
+x=re.search(terms,b)
+if x:
+    print("email is validate")
+else:
+    print("email is not validate")
 
 
